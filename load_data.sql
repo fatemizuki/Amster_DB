079728392db541b9382eb43f3ba4d2980e5e3247
fate_lingy <41952653+fatemizuki@users.noreply.github.com>
2020-12-02 21:03:53 +0800

    Create load_data.sql

---
+++ load_data.sql
@@ -0,0 +1,18 @@
+\cd :data_dir
+
+SET CLIENT_ENCODING TO 'WIN1252';
+
+-- admissions table
+\copy admissions FROM 'admissions.csv' DELIMITER ',' CSV HEADER NULL ''
+-- drugitems table
+\copy drugitems FROM 'drugitems.csv' DELIMITER ',' CSV HEADER NULL ''
+-- freetextitems
+\copy freetextitems FROM 'freetextitems.csv' DELIMITER ',' CSV HEADER NULL ''
+-- listitems
+\copy listitems FROM 'listitems.csv' DELIMITER ',' CSV HEADER NULL ''
+-- numericitems
+\copy numericitems FROM 'numericitems.csv' DELIMITER ',' CSV HEADER NULL ''
+-- procedureorderitems
+\copy procedureorderitems FROM 'procedureorderitems.csv' DELIMITER ',' CSV HEADER NULL ''
+-- processitems
+\copy processitems FROM 'processitems.csv' DELIMITER ',' CSV HEADER NULL ''
